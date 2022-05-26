7085ea1b7c516d2742fece612880c207f524d1fc
AnemaLite <106115876+AnemaLite@users.noreply.github.com>
2022-05-26 09:41:44 +0200

    Update fonctions.h

--- Projet_C/fonctions.h
+++ Projet_C/fonctions.h
@@ -9,8 +9,9 @@ int get_nbligne(char * filename);
 void parse(char* client_source, char* caisse_source);
 article_t creer_article(int x, int y);
 caisse_t *initialiser_caisse(char * ligne);
+caisse_t **initialiser_tableau_caisse(char * filename);
 client_t *initialiser_client(char * ligne);
-client_t **initialise_tableau_client(char* filename);
+client_t **initialiser_tableau_client(char* filename);
 void free_client(client_t * client);
 void free_tableau_client(client_t ** tableau_client);
 #endif

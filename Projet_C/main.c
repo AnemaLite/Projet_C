3d4f45c6743c0ce56e65b9e40c4a4b1731118977
AnemaLite <106115876+AnemaLite@users.noreply.github.com>
2022-05-26 15:17:16 +0200

    Update main.c

--- Projet_C/main.c
+++ Projet_C/main.c
@@ -5,24 +5,16 @@
 #include "fonctions.h"
 
 int main(int argc, char* argv[]){
-    //parse(argv[1],argv[2]);
-    int n = get_nbligne(argv[2]);
-    caisse_t* liste_caisses = malloc(sizeof(caisse_t)*n);
+    int n = get_nbligne(argv[1]);
+    caisse_t* liste_caisses = malloc(sizeof(caisse_t)*7);
     liste_caisses = initialiser_caisses(argv[2], liste_caisses);
-    for(int i = 0; i < 7; i++){
-        printf("numero : %d\ndebut : %d\nfin : %d\n",liste_caisses[i].numero,liste_caisses[i].debut,liste_caisses[i].fin);
-    }
+    afficher_caisses(liste_caisses);
 
-    client_t* liste_clients = malloc(sizeof(client_t)*get_nbligne(argv[1]));
+    client_t* liste_clients = malloc(sizeof(client_t)*n);
     liste_clients = initialiser_clients(argv[1], liste_clients);
-    for(int i = 0; i < get_nbligne(argv[1]); i++){
-        printf("id : %s\n entree : %d\n ",liste_clients[i].id, liste_clients[i].entree);
-    }
-    free(liste_clients);
+    afficher_clients(liste_clients,n);
+    liberer_clients(liste_clients,n);
     free(liste_caisses);
-    //client_t * client = initialiser_client((char*)"Utente1 59 4 S13 S21 S45 S52");
-    //caisse_t *caisse = initialiser_caisse("C1 1 14400");
-    //afficher_caisse(caisse);
-    //free(caisse);
     return 0;
 }
+

f57b738f7fdbcc27099a8e95226abf967fe54cd1
AnemaLite <106115876+AnemaLite@users.noreply.github.com>
2022-05-26 15:18:13 +0200

    Update fonctions.h

--- Projet_C/fonctions.h
+++ Projet_C/fonctions.h
@@ -3,17 +3,11 @@
 
 #include"structures.h"
 
-void afficher_client(client_t * client);
-void afficher_caisse(caisse_t * caisse);
+void afficher_clients(client_t * liste_client, int taille);
+void afficher_caisses(caisse_t * caisse);
 int get_nbligne(char * filename);
-void parse(char* client_source, char* caisse_source);
-article_t creer_article(int x, int y);
-caisse_t *initialiser_caisse(char * ligne);
-caisse_t **initialiser_tableau_caisse(char * filename);
-client_t *initialiser_client(char * ligne);
-client_t **initialiser_tableau_client(char* filename);
-void free_client(client_t * client);
-void free_tableau_client(client_t ** tableau_client);
 caisse_t* initialiser_caisses(char* caisses, caisse_t* liste_caisses);
 client_t* initialiser_clients(char* clients, client_t* liste_clients);
+void liberer_clients(client_t * liste_clients, int taille);
+
 #endif

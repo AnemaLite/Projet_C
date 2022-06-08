c23ee4bde0b07c1f4f9bbc41b0a91d7dfe39f2dc
AnemaLite <106115876+AnemaLite@users.noreply.github.com>
2022-06-08 07:31:25 +0200

    Update fonctions.h

--- Projet_C/fonctions.h
+++ Projet_C/fonctions.h
@@ -9,5 +9,11 @@ int get_nbligne(char * filename);
 caisse_t* initialiser_caisses(char* caisses, caisse_t* liste_caisses);
 client_t* initialiser_clients(char* clients, client_t* liste_clients);
 void liberer_clients(client_t * liste_clients, int taille);
+void liberer_caisses(caisse_t * liste_caisses);
+int temps_rayons(int i, int j, int i2, int j2);
+int temps_magasin(client_t client);
+void arrivee_caisse_client(client_t client, caisse_t* caisses, int* caisse, int* temps);
+
+
 
 #endif
